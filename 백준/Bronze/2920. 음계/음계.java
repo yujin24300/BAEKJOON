9970c01f920411bf439aeef526c59c6296360758
yujin24300 <128692567+yujin24300@users.noreply.github.com>
2023-07-11 23:14:49 +0900

    [Bronze II] Title: 음계, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        int[] X = new int[8];
+        String output = null;
+
+        for(int i = 0; i < X.length; i++)
+            X[i] = sc.nextInt();
+
+        for(int i = 0; i < X.length - 1; i++){
+            if(X[i + 1] == X[i] + 1)
+                output = "ascending";
+            else if(X[i + 1] == X[i] - 1)
+                output = "descending";
+            else{
+                output = "mixed";
+                break;
+            }
+        }
+
+        System.out.println(output);
+    }
+}
